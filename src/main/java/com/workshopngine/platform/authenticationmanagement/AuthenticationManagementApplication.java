49ca76054173c3ca3ebc54d10045337d6c0d71a5
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-02 18:58:31 -0500

    chore: init commit

---
+++ src/main/java/com/workshopngine/platform/authenticationmanagement/AuthenticationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.workshopngine.platform.authenticationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workshopngine/platform/authenticationmanagement/AuthenticationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshopngine.platform.authenticationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
