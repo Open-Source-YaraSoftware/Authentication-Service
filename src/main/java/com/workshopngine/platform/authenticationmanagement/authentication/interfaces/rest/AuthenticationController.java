fe97dfe8dc3d902b4ec6aa06522c69004d4b2d7a
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-02 22:07:04 -0500

    feat: add authentication controller
    Add sign up endpoint

---
+++ src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/interfaces/rest/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.workshopngine.platform.authenticationmanagement.authentication.interfaces.rest;
+
+import com.workshopngine.platform.authenticationmanagement.authentication.domain.services.UserCommandService;
+import com.workshopngine.platform.authenticationmanagement.authentication.interfaces.rest.dto.SignUpResource;
+import com.workshopngine.platform.authenticationmanagement.authentication.interfaces.rest.transform.SignUpCommandFromResourceAssembler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/authentication", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Authentication", description = "Authentication Endpoints")
+public class AuthenticationController {
+    private final UserCommandService userCommandService;
+
+    @PostMapping("/sign-up")
+    @Operation(summary = "Sign up", description = "Sign up a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created"),
+            @ApiResponse(responseCode = "400", description = "Bad request")
+    })
+    public ResponseEntity<?> signUp(@RequestBody SignUpResource resource) {
+        var command = SignUpCommandFromResourceAssembler.toCommandFromResource(resource);
+        userCommandService.handle(command);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
