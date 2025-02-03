ad0787fc573d759d9c8b787d363aee48d330a1c1
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-03 04:33:37 -0500

    feat:  add forgot password command

--- src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/domain/services/UserCommandService.java
+++ src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/domain/services/UserCommandService.java
@@ -1,9 +1,11 @@
 package com.workshopngine.platform.authenticationmanagement.authentication.domain.services;
 
 import com.workshopngine.platform.authenticationmanagement.authentication.domain.model.commands.AssignRoleCommand;
+import com.workshopngine.platform.authenticationmanagement.authentication.domain.model.commands.ForgotPasswordCommand;
 import com.workshopngine.platform.authenticationmanagement.authentication.domain.model.commands.SignUpCommand;
 
 public interface UserCommandService {
     void handle(SignUpCommand command);
     void handle(AssignRoleCommand command);
+    void handle(ForgotPasswordCommand command);
 }
