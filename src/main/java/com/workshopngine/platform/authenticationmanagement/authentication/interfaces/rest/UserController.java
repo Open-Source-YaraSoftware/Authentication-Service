e13a93873d43171f28d70dc09d2a8375dfda9123
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-03 04:05:28 -0500

    feat: add asssignRoleToUser method in user controller
    Add /users/{userId}/roles endpoint

--- src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/interfaces/rest/UserController.java
+++ src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/interfaces/rest/UserController.java
@@ -1,8 +1,11 @@
 package com.workshopngine.platform.authenticationmanagement.authentication.interfaces.rest;
 
 import com.workshopngine.platform.authenticationmanagement.authentication.domain.model.queries.GetUserByIdQuery;
+import com.workshopngine.platform.authenticationmanagement.authentication.domain.services.UserCommandService;
 import com.workshopngine.platform.authenticationmanagement.authentication.domain.services.UserQueryService;
+import com.workshopngine.platform.authenticationmanagement.authentication.interfaces.rest.dto.CreateAssignRoleResource;
 import com.workshopngine.platform.authenticationmanagement.authentication.interfaces.rest.dto.UserResource;
+import com.workshopngine.platform.authenticationmanagement.authentication.interfaces.rest.transform.AssignRoleCommandFromResourceAssembler;
 import com.workshopngine.platform.authenticationmanagement.authentication.interfaces.rest.transform.UserResourceFromEntityAssembler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -12,19 +15,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "User", description = "User Endpoints")
 public class UserController {
+    private final UserCommandService userCommandService;
     private final UserQueryService userQueryService;
 
-    @GetMapping("{userId}")
+    @GetMapping("/{userId}")
     @Operation(summary = "Get a user by id", description = "Get a user by id")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "User found"),
@@ -36,5 +37,17 @@ public class UserController {
         if (user.isEmpty()) return ResponseEntity.notFound().build();
         var userResource = UserResourceFromEntityAssembler.toResourceFromEntity(user.get());
         return new ResponseEntity<>(userResource, HttpStatus.OK);
+    }
+
+    @PostMapping({"/{userId}/roles"})
+    @Operation(summary = "Assign a role to a user", description = "Assign a role to a user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Role assigned"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
+    public ResponseEntity<?> assignRoleToUser(@PathVariable String userId, @RequestBody CreateAssignRoleResource roleName) {
+        var command = AssignRoleCommandFromResourceAssembler.toCommandFromResource(userId, roleName);
+        userCommandService.handle(command);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
