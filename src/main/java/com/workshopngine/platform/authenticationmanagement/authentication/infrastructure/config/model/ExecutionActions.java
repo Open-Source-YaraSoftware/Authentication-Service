cf1176b895566c2826d2f5624b9d43c20fd06bb7
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-03 04:32:35 -0500

    feat: add execution actions in variables to access directly

---
+++ src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/infrastructure/config/model/ExecutionActions.java
@@ -0,0 +1,5 @@
+package com.workshopngine.platform.authenticationmanagement.authentication.infrastructure.config.model;
+
+public class ExecutionActions {
+    public static final String UPDATE_PASSWORD = "UPDATE_PASSWORD";
+}
