479b65adb50eb74c6ed19641d4f1d29dcfade2bf
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-04 05:06:47 -0500

    feat: add values for grand types

--- src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/infrastructure/config/model/GranTypes.java
+++ src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/infrastructure/config/model/GranTypes.java
@@ -2,4 +2,7 @@ package com.workshopngine.platform.authenticationmanagement.authentication.infra
 
 public class GranTypes {
     public static final String REFRESH_TOKEN = "refresh_token";
+    public static final String GOOGLE_SUBJECT_TOKEN = "urn:ietf:params:oauth:grant-type:token-exchange";
+    public static final String GOOGLE_SUBJECT_TOKEN_TYPE = "urn:ietf:params:oauth:token-type:access_token";
+    public static final String GOOGLE_SUBJECT_ISSUER = "google";
 }
