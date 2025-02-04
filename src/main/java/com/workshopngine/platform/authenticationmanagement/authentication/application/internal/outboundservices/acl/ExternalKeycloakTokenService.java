fe8907c9cc82d7c462306a21d67de0fa078df438
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-03 19:27:30 -0500

    feat: add external keycloak token service
    Manage methods refresh token and get access token

---
+++ src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/application/internal/outboundservices/acl/ExternalKeycloakTokenService.java
@@ -0,0 +1,67 @@
+package com.workshopngine.platform.authenticationmanagement.authentication.application.internal.outboundservices.acl;
+
+import com.workshopngine.platform.authenticationmanagement.authentication.infrastructure.config.model.GranTypes;
+import com.workshopngine.platform.authenticationmanagement.authentication.infrastructure.config.model.KeycloakTokenResponse;
+import lombok.RequiredArgsConstructor;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.AccessTokenResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class ExternalKeycloakTokenService {
+    private final RestTemplate restTemplate;
+    @Value("${keycloak.auth-server-url}")
+    private String authServerUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Value("${keycloak.client-id}")
+    private String clientId;
+
+    @Value("${keycloak.client-secret}")
+    private String clientSecret;
+
+    public KeycloakTokenResponse refreshToken(String refreshToken) {
+        String tokenUrl = authServerUrl + "/realms/" + realm + "/protocol/openid-connect/token";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("grant_type", GranTypes.REFRESH_TOKEN);
+        formData.add("client_id", clientId);
+        formData.add("client_secret", clientSecret);
+        formData.add("refresh_token", refreshToken);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(formData, headers);
+
+        ResponseEntity<KeycloakTokenResponse> response = restTemplate.postForEntity(tokenUrl, request, KeycloakTokenResponse.class);
+
+        return response.getBody();
+    }
+
+    public AccessTokenResponse getAccessToken(String username, String password) {
+        Keycloak keycloak = KeycloakBuilder.builder()
+                .serverUrl(authServerUrl)
+                .realm(realm)
+                .grantType(OAuth2Constants.PASSWORD)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .username(username)
+                .password(password)
+                .build();
+        return keycloak.tokenManager().getAccessToken();
+    }
+}
