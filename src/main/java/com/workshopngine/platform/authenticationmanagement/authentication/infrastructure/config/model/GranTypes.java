a388732488b841b96e8320c5a804951940954589
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-03 19:24:43 -0500

    feat: add gran types values

---
+++ src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/infrastructure/config/model/GranTypes.java
@@ -0,0 +1,5 @@
+package com.workshopngine.platform.authenticationmanagement.authentication.infrastructure.config.model;
+
+public class GranTypes {
+    public static final String REFRESH_TOKEN = "refresh_token";
+}
