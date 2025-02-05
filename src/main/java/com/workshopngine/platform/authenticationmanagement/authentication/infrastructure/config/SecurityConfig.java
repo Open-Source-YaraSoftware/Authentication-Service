200730c7be0c6df59bd5e8d19a4be636671129c1
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-05 17:09:11 -0500

    feat(security-config): add jwtAuthenticationConverter

--- src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/workshopngine/platform/authenticationmanagement/authentication/infrastructure/config/SecurityConfig.java
@@ -3,11 +3,11 @@ package com.workshopngine.platform.authenticationmanagement.authentication.infra
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
@@ -51,6 +51,14 @@ public class SecurityConfig {
                 .successHandler(successHandler)
                 .failureUrl("/login?error=true"));
 
+        http.oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        return new JwtAuthenticationConverter();
     }
 }
